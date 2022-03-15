824f85b3ab3df7bc78083b3610b354d830036393
Avinash <avinash.kiran@assettl.com>
2022-03-15 10:35:04 +0530

    Home Page (15-03-2022)

--- app/src/main/java/com/example/kuver/ui/main/HomeActivity.kt
+++ app/src/main/java/com/example/kuver/ui/main/HomeActivity.kt
@@ -2,21 +2,56 @@ package com.example.kuver.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.text.SpannableString
+import android.text.style.AlignmentSpan
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.kuver.R
 import com.example.kuver.databinding.ActivityHomeBinding
 import com.example.kuver.databinding.ActivityLoginBinding
+import com.example.kuver.ui.main.fragment.HomeFragment
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
 
-
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUpViews()
+
     }
+
+    private fun setUpViews() {
+
+        setUpDrawerLayout()
+    }
+
+    private fun setUpDrawerLayout() {
+        setSupportActionBar(binding.homeToolbar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
---
+++ app/src/main/java/com/example/kuver/ui/main/fragment/HomeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.kuver.ui.main.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kuver.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): HomeFragment {
+            return HomeFragment()
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
